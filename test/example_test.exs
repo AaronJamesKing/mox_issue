1fe9dced133722d71be119bcd56b225baaa1e085
Aaron King <message.aaron.king@gmail.com>
2018-08-14 15:01:08 -0400

    Move the setup_all block into a CaseTemplate

--- test/example_test.exs
+++ test/example_test.exs
@@ -1,10 +1,5 @@
 defmodule ExampleTest do
-  use ExUnit.Case, async: false
-
-  setup_all do
-    Mox.stub_with(Dependency.Mock, Dependency)
-    :ok
-  end
+  use  Example.Case, async: false
 
   test "greets the world" do
     assert Example.hello() == :world
--- test/support/example_case.ex
+++ test/support/example_case.ex
@@ -1,5 +1,8 @@
 defmodule Example.Case do
   use ExUnit.CaseTemplate
 
-
+  setup_all do
+    Mox.stub_with(Dependency.Mock, Dependency)
+    :ok
+  end
 end
